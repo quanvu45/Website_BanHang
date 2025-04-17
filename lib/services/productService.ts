e21171a3b6b98a39ff87dfa8971b81ab1df57dd4
quanvu45 <nadarinojugen123@gmail.com>
2025-04-17 16:32:46 +0700

    fix productservice

--- lib/services/productService.ts
+++ lib/services/productService.ts
@@ -7,19 +7,19 @@ export const revalidate = 3600
 const getLatest = cache(async () => {
   await dbConnect()
   const products = await ProductModel.find({}).sort({ _id: -1 }).limit(6).lean()
-  return products as Product[]
+  return products as unknown as Product[]
 })
 
 const getFeatured = cache(async () => {
   await dbConnect()
   const products = await ProductModel.find({ isFeatured: true }).limit(3).lean()
-  return products as Product[]
+  return products as unknown as Product[]
 })
 
 const getBySlug = cache(async (slug: string) => {
   await dbConnect()
   const product = await ProductModel.findOne({ slug }).lean()
-  return product as Product
+  return product as unknown as Product
 })
 
 const PAGE_SIZE = 3
